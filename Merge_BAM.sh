391c46050cec59abe69eb0e65f916e3d3818f455
Emily Dittmar <Dittmare@gmail.com>
2019-10-08 15:18:35 -0400

    clean up merge bams script + add echo lines

--- Merge_BAM.sh
+++ Merge_BAM.sh
@@ -4,9 +4,9 @@ set -o pipefail
 
 ID=$(sed -n ${PBS_ARRAYID}p $ID_NAMES) #finds the IDname
 bams=$(find $MB_INPUTDIR -name "$ID[!0-9]*") #bam files with the same ID
-list=$(ls -1 $bams)
-echo "$list"
-samtools merge -nu $MB_OUTPUTDIR/${ID}.merged.bam $list
+echo "Merging $(ls -1 $bams | wc -l) bam files for sample $ID"
+echo "Bam files being merged are: $(ls -1 $bams)"
+samtools merge -nu $MB_OUTPUTDIR/${ID}.merged.bam $bams
 #java -jar picard.jar MergeSamFiles \
 #I=$list \
 #O=$MB_OUTPUTDIR/${ID}.merged.bam
