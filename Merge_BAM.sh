1288c047394cd18f98887d3b7c06f007dd64cfd8
Emily Dittmar <Dittmare@gmail.com>
2019-11-18 19:38:34 -0500

    Add functionality to "merge" handler for genotype coordinate sorted output files, and re-sort

--- Merge_BAM.sh
+++ Merge_BAM.sh
@@ -3,13 +3,18 @@
 set -o pipefail
 
 ID=$(sed -n ${PBS_ARRAYID}p $ID_NAMES) #finds the IDname
-bams=$(find $MB_INPUTDIR -name "$ID[!0-9]*Aligned.toTranscriptome.out.bam") #bam files with the same ID
-echo "Merging $(ls -1 $bams | wc -l) bam files for sample $ID"
-echo "Bam files being merged are: $(ls -1 $bams)"
-samtools merge -nu $MB_OUTPUTDIR/${ID}.merged.bam $bams
-#java -jar picard.jar MergeSamFiles \
-#I=$list \
-#O=$MB_OUTPUTDIR/${ID}.merged.bam
-#samtools merge -nu \
-#$MB_OUTPUTDIR/${ID}.merged.bam \
-#-b $list \
+
+if [[ "MB_FILETYPE" == "transcript" ]]; then
+	bams=$(find $MB_INPUTDIR -name "$ID[!0-9]*Aligned.toTranscriptome.out.bam") #bam files with the same ID
+	echo "Merging $(ls -1 $bams | wc -l) bam files for sample $ID"
+	echo "Bam files being merged are: $(ls -1 $bams)"
+	samtools merge -nu $MB_OUTPUTDIR/${ID}.merged.bam $bams
+elif [[ "MB_FILETYPE" == "genomic" ]]; then
+	bams=$(find $MB_INPUTDIR -name "$ID[!0-9]*Aligned.sortedByCoord.out.bam") #bam files with the same ID
+	echo "Merging $(ls -1 $bams | wc -l) bam files for sample $ID and then sorting"
+	echo "Bam files being merged are: $(ls -1 $bams)"
+	samtools merge -u $MB_OUTPUTDIR/${ID}.merged.bam $bams | samtools sort - -o sample.sorted.bam ###finish
+else
+	echo "Specify whether the bam files are transcript coordinates or genomic coordinates"
+	exit 1
+fi
