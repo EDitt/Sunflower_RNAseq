12e5ddd3f561aecee86f83fce9fe9731b26db376
Emily Dittmar <edittmar@uga.edu>
2020-04-04 17:29:12 -0400

    Create Split_NCigar.sh

---
+++ Split_NCigar.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o pipefail
+
+file=$(find $SN_INPUTDIR= -name "*${SN_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
+name=$(basename ${file%%$SN_SUFFIX}"") #sample ID
+
+echo "Splitting N-Cigar Reads for sample $name"
+
+gatk SplitNCigarReads \
+    -R ${GEN_FASTA} \
+    -I ${file} \
+    -O $SN_OUTPUTDIR/${name}_split.bam \
+    --tmp-dir ${TEMP_DIR}
