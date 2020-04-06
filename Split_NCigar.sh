b358321c5106baf2cee480bfb4849d83559fe950
Emily Dittmar <edittmar@uga.edu>
2020-04-06 12:22:23 -0400

    Fix typo

--- Split_NCigar.sh
+++ Split_NCigar.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-file=$(find $SN_INPUTDIR= -name "*${SN_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
+file=$(find $SN_INPUTDIR -name "*${SN_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
 name=$(basename ${file%%$SN_SUFFIX}"") #sample ID
 
 echo "Splitting N-Cigar Reads for sample $name"
