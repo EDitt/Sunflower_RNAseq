811d75f4c854a0a0e976c5bf667bc8a8dc063908
Emily Dittmar <edittmar@uga.edu>
2020-04-07 15:26:01 -0400

    Commenting out statements

--- Filter_Junctions.sh
+++ Filter_Junctions.sh
@@ -31,8 +31,8 @@ else
 	echo "Removing ${SCAFFOLDJUNC} junctions from scaffold sequence"
 	if [[ "$REMOVE_NC_JUNC" == "yes" ]]; then
 		echo "Removing ${NCJUNC} non-canonical junctions"
-		TOTREM=$(cat ${JUNCTIONDIR}/*SJ.out.tab | awk -v var="$SCAFFOLD_STRING" '($1 !~ var && $5 > 0 && $6==0)' | cut -f1-6 | sort | uniq | wc -l) # total not annotated, not in scaffold, canonical
-		echo "Removing annotated, non-canonical, and scaffold junctions left ${TOTREM} junctions"
+		#TOTREM=$(cat ${JUNCTIONDIR}/*SJ.out.tab | awk -v var="$SCAFFOLD_STRING" '($1 !~ var && $5 > 0 && $6==0)' | cut -f1-6 | sort | uniq | wc -l) # total not annotated, not in scaffold, canonical
+		#echo "Removing annotated, non-canonical, and scaffold junctions left ${TOTREM} junctions"
 		cat ${JUNCTIONDIR}/*SJ.out.tab | awk -v var="$SCAFFOLD_STRING" '($1 !~ var && $5 > 0 && $6==0)' | awk -v num="$UNIQUE_NUM" '($7>=num || ++a[$1,$2,$3,$4,$5,$6]==num)' | cut -f1-6 | sort | uniq  > ${JUNCTIONDIR}/${SJ_LISTNAME}_SJ.filtered.tab
 		
 	else
