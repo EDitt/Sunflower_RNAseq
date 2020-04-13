02d4fcd513e3f9f134203286ca1cac0d37031915
Emily Dittmar <edittmar@uga.edu>
2020-04-13 18:09:25 -0400

    Combine BAM processing steps

---
+++ BAM_Processing.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o pipefail
+
+if [[ ! -d "$TEMP_DIR" ]]; then
+	echo "Please specify a temporary directory, exiting..."
+	exit 1
+fi
+
+file=$(find $PB_INPUTDIR -name "*${PB_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
+name=$(basename ${file%%$PB_SUFFIX}"") #sample ID
+
+echo "Marking Duplicates for sample $name"
+
+java -jar ${PICARD_JAR} MarkDuplicates \
+	-I ${file} \
+    -O $TEMP_DIR/${name}DupsMarked.bam \
+    -M $PB_OUTPUTDIR/${name}Duplicate_metrics.txt
+
+echo "Splitting N-Cigar Reads for sample $name"
+
+gatk SplitNCigarReads \
+    -R ${GEN_FASTA} \
+    -I $TEMP_DIR/${name}DupsMarked.bam \
+    -O $PB_OUTPUTDIR/${name}processed.bam \
+    --tmp-dir ${TEMP_DIR}
+
+rm $TEMP_DIR/${name}DupsMarked.bam
---
+++ Validate_BAMs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o pipefail
+
+for file in `find $VB_INPUTDIR -name "*${VB_SUFFIX}"`; do
+	name=$(basename ${file%%$VB_SUFFIX}"") #sample ID
+	echo "Validating $name BAM file"
+	java -jar ${PICARD_JAR} ValidateSamFile \
+	-I ${file} \
+	-MODE SUMMARY \
+	-O $VB_OUTPUTDIR/BAMStats #does this append in a loop?
+done
