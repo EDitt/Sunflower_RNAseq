de521116ab382d6d238fe9e64a3f2f5d01931aab
Emily Dittmar <edittmar@uga.edu>
2020-04-20 13:17:55 -0400

    This v. of Picard takes diff flag syntax

--- Validate_BAM.sh
+++ Validate_BAM.sh
@@ -6,7 +6,7 @@ for file in `find $VB_INPUTDIR -name "*${VB_SUFFIX}"`; do
 	name=$(basename ${file%%$VB_SUFFIX}"") #sample ID
 	echo "Validating $name BAM file"
 	java -jar ${PICARD_JAR} ValidateSamFile \
-	-I ${file} \
-	-MODE SUMMARY \
-	-O $VB_OUTPUTDIR/BAMStats #does this append in a loop?
+	I=${file} \
+	MODE=SUMMARY \
+	O=$VB_OUTPUTDIR/BAMStats #does this append in a loop?
 done
