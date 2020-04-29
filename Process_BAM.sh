42a2ee32180fa8fe315adaeee0207830f86e6a27
Emily Dittmar <edittmar@uga.edu>
2020-04-29 00:46:58 -0400

    Syntax

--- Process_BAM.sh
+++ Process_BAM.sh
@@ -13,16 +13,16 @@ name=$(basename ${file%%$PB_SUFFIX}"") #sample ID
 echo "Marking Duplicates for sample $name"
 
 java -jar ${PICARD_JAR} MarkDuplicates \
-	I=${file} \
-    O=$TEMP_DIR/${name}DupsMarked.bam \
-    M=$PB_OUTPUTDIR/${name}Duplicate_metrics.txt
+	I="${file}" \
+    O="${TEMP_DIR}/${name}DupsMarked.bam" \
+    M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
 
 echo "Splitting N-Cigar Reads for sample $name"
 
 gatk SplitNCigarReads \
-	-R ${GEN_FASTA} \
-	-I $TEMP_DIR/${name}DupsMarked.bam \
-	-O $PB_OUTPUTDIR/${name}processed.bam \
-	--tmp-dir ${TEMP_DIR}
+	-R "${GEN_FASTA}" \
+	-I "${TEMP_DIR}/${name}DupsMarked.bam" \
+	-O "${PB_OUTPUTDIR}/${name}processed.bam" \
+	--tmp-dir "${TEMP_DIR}"
 
 rm $TEMP_DIR/${name}DupsMarked.bam
