6c706613837134809b47a51c79969e8e3591a15a
Emily Dittmar <edittmar@uga.edu>
2020-05-25 17:10:51 -0400

    debugging

--- Process_BAM.sh
+++ Process_BAM.sh
@@ -10,19 +10,19 @@ fi
 file=$(find $PB_INPUTDIR -name "*${PB_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
 name=$(basename ${file%%$PB_SUFFIX}"") #sample ID
 
-echo "Marking Duplicates for sample $name"
+#echo "Marking Duplicates for sample $name"
 
-java -jar ${PICARD_JAR} MarkDuplicates \
-	I="${file}" \
-    O="${PB_OUTPUTDIR}/${name}DupsMarked.bam" \
-    M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
+#java -jar ${PICARD_JAR} MarkDuplicates \
+#	I="${file}" \
+#    O="${PB_OUTPUTDIR}/${name}DupsMarked.bam" \
+#    M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
 
-#echo "Splitting N-Cigar Reads for sample $name"
+echo "Splitting N-Cigar Reads for sample $name"
 
-#gatk SplitNCigarReads \
-#	-R "${GEN_FASTA}" \
-#	-I "${TEMP_DIR}/${name}DupsMarked.bam" \
-#	-O "${PB_OUTPUTDIR}/${name}processed.bam" \
-#	--tmp-dir "${TEMP_DIR}"
+gatk SplitNCigarReads \
+	-R "${GEN_FASTA}" \
+	-I "${PB_OUTPUTDIR}/${name}DupsMarked.bam" \
+	-O "${PB_OUTPUTDIR}/${name}processed.bam" \
+	--tmp-dir "${TEMP_DIR}"
 
-#rm $TEMP_DIR/${name}DupsMarked.bam
+#rm ${PB_OUTPUTDIR}/${name}DupsMarked.bam
