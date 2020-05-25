bca2f02a1c0ef187ba8636d4eaa6d154688b22cd
Emily Dittmar <edittmar@uga.edu>
2020-05-25 14:27:57 -0400

    updata java options

--- Process_BAM.sh
+++ Process_BAM.sh
@@ -12,7 +12,7 @@ name=$(basename ${file%%$PB_SUFFIX}"") #sample ID
 
 echo "Marking Duplicates for sample $name"
 
-java -Xmx2g -jar ${PICARD_JAR} MarkDuplicates \
+java -Xmx12g -jar ${PICARD_JAR} MarkDuplicates \
 	I="${file}" \
     O="${TEMP_DIR}/${name}DupsMarked.bam" \
     M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
