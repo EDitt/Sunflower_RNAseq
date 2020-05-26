54f6db901255281b002a72f9647127492626fa39
Emily Dittmar <edittmar@uga.edu>
2020-05-25 22:08:31 -0400

    remove commented-out lines used for debugging

--- SNP_calling/Process_BAM.sh
+++ SNP_calling/Process_BAM.sh
@@ -10,12 +10,12 @@ fi
 file=$(find $PB_INPUTDIR -name "*${PB_SUFFIX}" | sed -n ${PBS_ARRAYID}p) #finds the file
 name=$(basename ${file%%$PB_SUFFIX}"") #sample ID
 
-#echo "Marking Duplicates for sample $name"
+echo "Marking Duplicates for sample $name"
 
-#java -jar ${PICARD_JAR} MarkDuplicates \
-#	I="${file}" \
-#    O="${PB_OUTPUTDIR}/${name}DupsMarked.bam" \
-#    M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
+java -jar ${PICARD_JAR} MarkDuplicates \
+	I="${file}" \
+    O="${PB_OUTPUTDIR}/${name}DupsMarked.bam" \
+    M="${PB_OUTPUTDIR}/${name}Duplicate_metrics.txt"
 
 echo "Splitting N-Cigar Reads for sample $name"
 
@@ -25,4 +25,4 @@ gatk SplitNCigarReads \
 	-O "${PB_OUTPUTDIR}/${name}processed.bam" \
 	--tmp-dir "${TEMP_DIR}"
 
-#rm ${PB_OUTPUTDIR}/${name}DupsMarked.bam
+rm ${PB_OUTPUTDIR}/${name}DupsMarked.bam
