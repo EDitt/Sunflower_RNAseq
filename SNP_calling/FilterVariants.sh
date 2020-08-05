6990b83a363923ce8f66a37b0772ae0b8b57335a
Emily Dittmar <edittmar@uga.edu>
2020-08-05 17:07:53 -0400

    variant filtration

--- SNP_calling/FilterVariants.sh
+++ SNP_calling/FilterVariants.sh
@@ -2,11 +2,24 @@
 
 set -o pipefail
 
- gatk VariantFiltration \
-   -R "${GEN_FASTA}" \
-   -V input.vcf.gz \
-   -O output.vcf.gz \
-   --filter-name "my_filter1" \
-   --filter-expression "AB < 0.2" \
-   --filter-name "my_filter2" \
-   --filter-expression "MQ0 > 50"
+mkdir -p "${CV_OUTPUTDIR}/Intermediates"
+
+gatk IndexFeatureFile \
+     -F "${VCF_INPUT}"
+
+gatk VariantFiltration \
+	-R "${GEN_FASTA}" \
+   	-V "${VCF_INPUT}" \
+   	--cluster-window-size 35 \
+   	--cluster-size 3 \
+   	-filter-name FS --filter-expression "FS > ${FSV}" \
+   	-filter-name QD --filter-expression "QD < ${QDV}" \
+   	-O "${CV_OUTPUTDIR}/Intermediates/${PROJECT}_filters.vcf"
+
+vcftools --vcf "${CV_OUTPUTDIR}/Intermediates/${PROJECT}_filters.vcf" \
+--remove-filtered-all \
+--recode \
+--recode-INFO-all \
+--out "${CV_OUTPUTDIR}/${PROJECT}_filtered"
+
+
